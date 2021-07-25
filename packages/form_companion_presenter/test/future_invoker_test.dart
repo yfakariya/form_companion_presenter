40c9bc5fc977a4a777a33af42016d7e1081ee65c
yfakariya <yfakariya@gmail.com>
2021-07-25 20:56:27 +0900

    test: add test cases

--- packages/form_companion_presenter/test/future_invoker_test.dart
+++ packages/form_companion_presenter/test/future_invoker_test.dart
@@ -261,6 +261,104 @@ void main() {
               ),
             );
           });
+
+          group(
+              '2 request with same parameter only early one is executed and set to result.',
+              () {
+            Future doTest(Future<String> Function(String) future) async {
+              String? result1;
+              String? result2;
+              String? result3;
+
+              final parameter1 = Parameter<String, void>(
+                  value: 'input1', onCompleted: (r) => result1 = r);
+              final parameter2 = Parameter<String, void>(
+                  value: 'input1', onCompleted: (r) => result2 = r);
+              final parameter3 = Parameter<String, void>(
+                  value: 'input2', onCompleted: (r) => result3 = r);
+              const expected = 'output';
+              const defaultResult = 'default';
+
+              var called = 0;
+              final target = TestTarget<String, void>(
+                callback: (p) {
+                  printOnFailure('future called with $p');
+                  called++;
+                  return future(expected + p.value);
+                },
+                defaultResult: defaultResult,
+              );
+
+              // Assert precondition
+              expect(target.status, equals(AsyncOperationStatus.initial));
+
+              // Assert before the future
+              expect(target.execute(parameter1), equals(defaultResult));
+              expect(target.status, equals(AsyncOperationStatus.inProgress));
+              expect(target.execute(parameter2), equals(defaultResult));
+              expect(target.status, equals(AsyncOperationStatus.inProgress));
+
+              printOnFailure('await parameter1.completer');
+              await parameter1.completer.future;
+
+              // Note that parameter2.completer should never signaled.
+              // Assert counter -- 1 rather than 2 because parameter2 was ignored.
+              expect(called, equals(1));
+
+              // Assert status after the future
+              expect(target.status, equals(AsyncOperationStatus.completed));
+
+              // Assert callback behaviors
+              expect(parameter1.isOnCompletedCalled, isTrue);
+              expect(parameter1.isOnFailedCalled, isFalse);
+              expect(parameter1.isOnProgressCalled, isFalse);
+              expect(parameter2.isOnCompletedCalled, isFalse);
+              expect(parameter2.isOnFailedCalled, isFalse);
+              expect(parameter2.isOnProgressCalled, isFalse);
+
+              expect(result1, equals('$expected${parameter1.value}'));
+              expect(result2, isNull);
+
+              // We can get cached result.
+              expect(target.execute(parameter2),
+                  equals('$expected${parameter2.value}'));
+
+              // These extra invocation actually 2 cases
+              // 1) parameter2 is not executed rather than just waited to be
+              //    called by assert that parameter3 is called.
+              // 2) the FutureInvoker can handle additional execute() call,
+              //    inProgress -> completed state transition.
+
+              expect(target.execute(parameter3),
+                  equals('$expected${parameter2.value}'));
+              expect(target.status, equals(AsyncOperationStatus.inProgress));
+
+              printOnFailure('await parameter3.completer');
+              await parameter3.completer.future;
+
+              // Assert counter -- 2 rather than 3 because parameter2 was ignored.
+              expect(called, equals(2));
+
+              // Assert status after the future
+              expect(target.status, equals(AsyncOperationStatus.completed));
+
+              // Assert callback behaviors
+              expect(parameter1.isOnCompletedCalled, isTrue);
+              expect(parameter1.isOnFailedCalled, isFalse);
+              expect(parameter1.isOnProgressCalled, isFalse);
+              expect(parameter2.isOnCompletedCalled, isFalse);
+              expect(parameter2.isOnFailedCalled, isFalse);
+              expect(parameter2.isOnProgressCalled, isFalse);
+              expect(parameter3.isOnCompletedCalled, isTrue);
+              expect(parameter3.isOnFailedCalled, isFalse);
+              expect(parameter3.isOnProgressCalled, isFalse);
+
+              expect(result3, equals('$expected${parameter3.value}'));
+
+              // We can get cached result.
+              expect(target.execute(parameter3),
+                  equals('$expected${parameter3.value}'));
+              expect(target.status, equals(AsyncOperationStatus.completed));
             }
 
             test(
@@ -279,11 +377,97 @@ void main() {
               ),
             );
           });
-          // TODO(yfakariya): same parameter twice
-          // TODO(yfakariya): completed -> inprogress
         });
 
-        // TODO(yfakariya): failed
+        group('failure', () {
+          group('1 request is executed asynchronously and remember exception.',
+              () {
+            Future doTest(Future<String> Function(Exception) future) async {
+              AsyncError? failure1;
+              final parameter = Parameter<String, void>(
+                  value: 'input', onFailed: (f) => failure1 = f);
+              const defaultResult = 'default';
+              final error = Exception('DUMMY');
+
+              Parameter<String, void>? passed;
+              final target = TestTarget<String, void>(
+                callback: (p) {
+                  passed = p;
+                  return future(error);
+                },
+                defaultResult: defaultResult,
+              );
+
+              // Assert precondition
+              expect(target.status, equals(AsyncOperationStatus.initial));
+
+              // Assert before the future
+              expect(target.execute(parameter), equals(defaultResult));
+              expect(target.status, equals(AsyncOperationStatus.inProgress));
+
+              printOnFailure('${target.status}');
+              try {
+                await parameter.completer.future;
+                fail('No exception thrown.');
+              } on Exception catch (e) {
+                expect(e, same(error));
+              }
+
+              // Assert mock
+              expect(passed, same(parameter));
+
+              // Do message pump to execute catch on 'sync' case...
+              await Future<void>.delayed(Duration.zero);
+
+              // Assert status after the future
+              expect(target.status, equals(AsyncOperationStatus.failed));
+
+              // Assert callback behaviors
+              expect(parameter.isOnCompletedCalled, isFalse);
+              expect(parameter.isOnFailedCalled, isTrue);
+              expect(parameter.isOnProgressCalled, isFalse);
+
+              expect(failure1, isNotNull);
+              expect(failure1?.error, isNotNull);
+              expect(failure1?.stackTrace, isNotNull);
+              expect(failure1?.error, same(error));
+
+              // We can get cached exception.
+
+              try {
+                target.execute(parameter);
+              }
+              // ignore: avoid_catching_errors
+              on AsyncError catch (e) {
+                expect(e, isA<AsyncError>());
+                expect(e.error, same(error));
+                expect(e.stackTrace, isNotNull);
+              }
+
+              expect(target.status, equals(AsyncOperationStatus.failed));
+            }
+
+            test(
+              'async',
+              () => doTest(
+                (e) async {
+                  await Future<void>.delayed(Duration.zero, () => throw e);
+                  return 'ERROR';
+                },
+              ),
+            );
+
+            test(
+              'sync',
+              () => doTest(
+                (e) {
+                  throw e;
+                },
+              ),
+            );
+          });
+        });
+
         // TODO(yfakariya): failed -> failed -> throw
         // TODO(yfakariya): failed -> throw
         // TODO(yfakariya): failed -> inprogress
