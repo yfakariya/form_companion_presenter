5901a4f10f9d84b7da0c2826b518867138a56e4c
yfakariya <yfakariya@gmail.com>
2021-11-10 21:03:24 +0900

    chore: stabilize sample AsyncValidationIndicator

--- examples/form_companion_presenter_example/lib/async_validation_indicator.dart
+++ examples/form_companion_presenter_example/lib/async_validation_indicator.dart
@@ -1,11 +1,12 @@
 // See LICENCE file in the root.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:form_companion_presenter/form_companion_presenter.dart';
 
 class AsyncValidationIndicator extends StatelessWidget {
-  final bool Function() _asyncValidationStateProvider;
+  final ValueListenable<bool> _hasPendingAsyncValidationListener;
   final String? text;
   final double? height;
 
@@ -15,31 +16,32 @@ class AsyncValidationIndicator extends StatelessWidget {
     required String propertyName,
     this.text,
     this.height,
-  })  : _asyncValidationStateProvider =
-            (() => presenter.hasPendingAsyncValidations(propertyName)),
+  })  : _hasPendingAsyncValidationListener =
+            presenter.getPropertyPendingAsyncValidationsListener(propertyName),
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (!_asyncValidationStateProvider()) {
-      return const SizedBox(width: 0);
-    }
-
     final indicatorSize =
         height ?? Theme.of(context).textTheme.subtitle1?.height ?? 16;
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        SizedBox(
-          width: indicatorSize,
-          height: indicatorSize,
-          child: const CircularProgressIndicator(),
-        ),
-        const SizedBox(
-          width: 4,
-        ),
-        Text(text ?? 'Validating...'),
-      ],
+    return ValueListenableBuilder<bool>(
+      valueListenable: _hasPendingAsyncValidationListener,
+      builder: (_, hasPendingAsyncValidation, indicator) =>
+          hasPendingAsyncValidation ? indicator! : const SizedBox(width: 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            width: indicatorSize,
+            height: indicatorSize,
+            child: const CircularProgressIndicator(),
+          ),
+          const SizedBox(
+            width: 4,
+          ),
+          Text(text ?? 'Validating...'),
+        ],
+      ),
     );
   }
 }
