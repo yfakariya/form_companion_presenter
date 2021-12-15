298e3ff4acfa204dcbfd098dfbc7baca6862382e
yfakariya <yfakariya@gmail.com>
2021-12-15 18:38:05 +0900

    fix: update flutter_form_builder version
    This commit also includes example's package version update.

--- packages/form_builder_companion_presenter/example/lib/bulk_auto_validation_form_builder_account.dart
+++ packages/form_builder_companion_presenter/example/lib/bulk_auto_validation_form_builder_account.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:form_builder_companion_presenter/form_builder_companion_presenter.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:form_companion_presenter/async_validation_indicator.dart';
 import 'package:form_companion_presenter/form_companion_presenter.dart';
 import 'package:meta/meta.dart';
